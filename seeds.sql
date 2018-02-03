251efb385a16f7ff55398aab90b6f4f17bd752ae
unknown <londonvayavong@gmail.com>
2018-02-03 12:58:00 -0800

    fixed mysql

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,16 @@
-
 USE bamazon;
 
 
-INSERT INTO products (name)
-VALUES ("Thor Ragnarok", "Movie", 19.99, 600),
-("Guardians of the Galaxy Vol 2", "Movie", 17.99, 400),
-("Ed Wood", 5.00, 50),("Willow", "Movie", 10.00, 500),
-("Captain America Civil War", 'Movie', 8.00, 800),
-("Star Wars Force Awakens", 'Movie', 15.00, 800),
-("What If", 'Movie', 7.00, 50),
-("Paddington", 'Movie', 100, 30),
-("Jerusalem", 'Novel', 26.00, 300),
-("Neverwhere", 'Novel', 9.00, 400) ;
+INSERT INTO products
+    (name)
+VALUES
+    ("Thor Ragnarok", "movie", 19.99, 600),
+    ("Guardians of the Galaxy Vol 2", "movie", 17.99, 400),
+    ("Ed Wood", "movie", 5.00, 50),
+    ("Willow", "movie", 10.00, 500),
+    ("Captain America Civil War", "movie", 8.00, 800),
+    ("Star Wars Force Awakens", "movie", 15.00, 800),
+    ("What If", "movie", 7.00, 50),
+    ("Paddington", "movie", 100, 30),
+    ("Jerusalem", "novel", 26.00, 300),
+    ("Neverwhere", "novel", 9.00, 400);
